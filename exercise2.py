5d93bae609061f9cc959c7f0955ddb80db3c9964
Hussein Mahdi <hussein_realmadrid@yahoo.com>
2017-02-16 13:31:06 +0000

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+2. Variables
+Declare two variables: one string which will contain your name on and one integer which will represent your age. Print both variables with an appropriate message.
+
+Convert your age to floating point using float(x) (x represents the integer’s value)
+
+#2. Variables
+name = "Hussein"
+age = "22"
+print (name + " " + age)
+
+print(float(age))
