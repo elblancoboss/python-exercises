af1770f8a6608e4932c7d2fd2980644ac4ae26f6
Hussein Mahdi <hussein_realmadrid@yahoo.com>
2017-02-16 13:31:36 +0000

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,17 @@
+3. Lists
+Tip: Have a look at this excellent tutorial in python lists: http://www.tutorialspoint.com/python/python_lists.htm
+•	Create a list that contains several random attributes:
+Example:
+“abcd”, “jko”, “278”, “18 Feb 2016”
+
+•	Print the list contents
+
+•	Add some duplicate elements.
+Example:
+“abcd”, “abcd”, “jko”, ...
+
+#3. Lists
+list = ['abcd', 'jko', 278, '18 Feb 2016'];
+print(list)
+
+print(list[0:2]*2, list[3]*2)
