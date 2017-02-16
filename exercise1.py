a857d16ef3cf358614b8899ad56ddc60f7b42851
Hussein Mahdi <hussein_realmadrid@yahoo.com>
2017-02-16 13:30:26 +0000

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,22 @@
+1. Introduction
+
+Open the python terminal and enter the following line to the command prompt:
+1)	print "Hello World!"
+2)	print "Hello Again"
+
+Can you notice its difference with ‘traditional’ languages like Java, c++, etc.?
+
+Make your script print another line.
+
+Make your script print only one line.
+
+Put a '#' character at the beginning of a line. What did it do? Try to find out what this character does
+
+#1. Introduction
+print ("Hello World!")
+print ("Hello Again")
+
+s = "Hello World!", "Hello Again"
+print(s)
+
+#print ("hello world")
